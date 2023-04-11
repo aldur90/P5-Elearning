3207ac707e62565e13a3dab33b3bd42f13ee427e
aldur90 <alandurannoce@gmail.com>
2023-04-11 17:41:05 -0300

    getunit completed

--- force-app/main/default/lwc/unitContent/unitContent.js
+++ force-app/main/default/lwc/unitContent/unitContent.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getUnitWrapper from '@salesforce/apex/UnitService.getUnit';
-import registerUserAnswer from '@salesforce/apex/JSController.registerUserAnswer';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import registerUserAnswer from '@salesforce/apex/UnitService.registerUnitHistory';
 
 export default class UnitContent extends LightningElement {
     @api recordId
@@ -9,17 +10,23 @@ export default class UnitContent extends LightningElement {
     name;
     points;
     time;
-	isCompletedUnit;
+	isCompletedUnit=false;
+
+    connectedcallback(){ console.log(isCompletedUnit) };
 
     @wire(getUnitWrapper, {unitId:'$recordId'})
     unitdata(Result){
         const { data, error } = Result;
 
         if(data){
+            console.log('Completed ' + data);
             this.unit = data.unit;
             this.questions = data.questions;
             this.points = data.unit.Points__c;
             this.time = data.unit.Time_Estimate__c;
+            this.isCompletedUnit = data.completed;
+            //data.unit.Unit_Responses__r[0].Unit_Completed__c;
+            console.log('Unidad Completada ' + this.isCompletedUnit);
 
         }   else if (error) {
             this.error = error;
@@ -61,14 +68,45 @@ export default class UnitContent extends LightningElement {
 
         registerUserAnswer({ unitId: this.recordId, jsonAnswer: _jsonAnswer})
 		.then((result) => {
-			console.log('succesfully');
-			this.isCompletedUnit = result
+			console.log('succesfully ' + result);
+			this.isCompletedUnit = result;
+            if(this.isCompletedUnit){
+                this.showSuccessToast();               
+            }else{
+                this.showErrorToast();
+                
+            }
+            //if(this.isCompletedUnit){
+            //    this.showSuccessToast();
+            //}else{
+             //   showErrorToast();
+            //}
 		})
         .catch((error)=>{
 			console.log('has an error');
             console.log(error)
         });
 		console.log(this.isCompletedUnit);
+        
     }
 
+    showErrorToast() {
+        const evt = new ShowToastEvent({
+            title: 'Fail',
+            message: 'Some unexpected error',
+            variant: 'error',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
+    }
+    
+    showSuccessToast() {
+        const evt = new ShowToastEvent({
+            title: 'Toast Success',
+            message: 'Opearion sucessful',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
+    }
 }
